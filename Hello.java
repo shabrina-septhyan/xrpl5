d0770bacc005954c18be106147b79c001983e87c
Shabrina <shabrina_nuraini_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:15:26 +0700

    Update

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        String nama = "Shabrina";
+        String alamat = "sidoarjo";
+        int usia = 15;
+        char jenisKelamin = 'p';
+        double beratBadan = 30.00;
+
+        System.out.println(nama);
+
+    }
+}
---
+++ Tes.java
@@ -0,0 +1,11 @@
+/**
+ * Tes
+ */
+public class Tes {
+
+  public static void main(String[] args) {
+    String nama = "shabrina";
+    String kelas = "X RPL 5";
+    System.out.println("Halo, nama saya " + nama + " Dari kelas " + kelas);
+  }
+}
